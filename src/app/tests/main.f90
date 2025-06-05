84ec30bbed4a2b2bbb80cb63587029a205cd2776
davidpfister <david.pfister@ypso-facto.com>
2025-06-05 12:20:40 +0200

    add listdirected option in args udtio

--- src/app/tests/main.f90
+++ src/app/tests/main.f90
@@ -37,21 +37,39 @@ console(test)
         
         cl = '"abc" d e'
         args = split_commandline_into_args(cl)
+        write(*, *) args
 
         cl = 'a\\b d"e f"g h'
         args = split_commandline_into_args(cl)
+        write(*, *) args
 
         cl = 'a\\\"b c d'
         args = split_commandline_into_args(cl)
+        write(*, *) args
 
         cl = 'a\\\\"b c" d e'
         args = split_commandline_into_args(cl)
-
+        write(*, *) args
+        
         cl = 'a"b"" c d'
         args = split_commandline_into_args(cl)
+        write(*, *) args
         
         cl = '--d "test\"'
         args = split_commandline_into_args(cl)
+        write(*, *) args
+        
+        cl = '-d "a, b, c"'
+        args = split_commandline_into_args(cl)
+        write(*, *) args
+        
+        cl = """test path"" -d ""a, b, c"""
+        args = split_commandline_into_args(cl)
+        write(*, *) args
+        
+        cl = """test path"" -d 'a, b, c'"
+        args = split_commandline_into_args(cl)
+        write(*, *) args
 
     end subroutine
 end
